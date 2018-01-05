ac7f0317814bf1dba371a383dfb6f7dc0624a253
Tonis Ormisson <tonis@andmemasin.eu>
2018-01-05 15:24:35 +0200

    scroll to first unanswered mandatory

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,5 +1,5 @@
 
-
+var globalI = 0;
 // check for unanswered mandatory on any page load
 $( document ).ready(function() {
     hilightQuestionErrors();
@@ -11,11 +11,34 @@ $(document).on("ready pjax:scriptcomplete", function(){
 
 // check and hilight all unanswered questions
 function hilightQuestionErrors() {
+    var hasErrors = false;
+    var firstErrorQuestionContainer;
+
     $( ".ls-question-mandatory" ).each(function( i, obj ) {
+        if(i === 0){
+            hasErrors = true;
+            firstErrorQuestionContainer = $(this).closest(".question-container");
+        }
+        globalI +=1;
+
         $( this )
             .closest(".question-container")
             .addClass("question-hilight");
     });
 
+    if(hasErrors){
+        console.log(firstErrorQuestionContainer);
+        var navHeight = $('.navbar').outerHeight();
+        console.log(navHeight);
+        console.log(firstErrorQuestionContainer.offset().top),
+
+
+        $('html, body').animate({
+            scrollTop: firstErrorQuestionContainer.offset().top - navHeight
+        }, 50);
+    }
+
 };
+
+
 
