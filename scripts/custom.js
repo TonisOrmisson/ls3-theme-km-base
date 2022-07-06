fcdf1f6f531bde2c0eba7106a144f69ee5846e4c
tonis ormisson <tonis@andmemasin.eu>
2022-07-06 13:04:36 +0300

    add maxdiff option fo js & css

--- scripts/custom.js
+++ scripts/custom.js
@@ -17,3 +17,60 @@ function togglesDiv(divsId){
         catdiv.style.display = "";
     }
 }
+
+
+function maxDiff(qID, randomize) {
+
+    // Identify some elements
+    var thisQuestion = $('#question'+qID);
+    var thisTable = $('table.subquestion-list:eq(0)', thisQuestion);
+
+    // Assign a new question class
+    $(thisQuestion).addClass('max-diff-array');
+
+    // Move the columns
+    $('thead tr:eq(0)', thisTable).prepend($('thead tr:eq(0) th:eq(1)', thisTable));
+    $('tr.answers-list', thisTable).each(function(i){
+        $('td.answer-item:eq(0)', this).prependTo(this);
+    });
+
+    // Random rows
+    if(randomize) {
+        var rowsArr = [];
+        $('tr.answers-list', thisTable).each(function(i){
+            $(this).attr('data-index', i);
+            rowsArr.push(i);
+        });
+        shuffleArray(rowsArr);
+        $(rowsArr).each(function(i){
+            $('tbody', thisTable).append($('tr[data-index="'+this+'"]', thisTable));
+        });
+    }
+
+    // Prevent clicking twice in the same row
+    $('input.radio', thisQuestion).on('click', function () {
+
+        $('input.radio', thisQuestion).prop('disabled', false);
+        $('input.radio:checked', thisQuestion).each(function(i) {
+            var thisRow = $(this).closest('tr.answers-list');
+            $('input.radio', thisRow).not(this).prop('disabled', true);
+        });
+    });
+
+    // Fix up the row classes
+    var rowClass = 1;
+    $('tr.answers-list', thisTable).each(function(i) {
+        $(this).addClass('array'+(2-(i%2)));
+    });
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
