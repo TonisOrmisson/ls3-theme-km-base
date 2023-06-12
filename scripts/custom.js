e16a0c87171d8e6ca2a637d36e935b7bdac9f927
tonis <tonis@andmemasin.eu>
2023-06-12 15:49:29 +0300

    fixed missing theme.js (v5)

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,3 +1,12 @@
+
+
+$(document).on('ready pjax:scriptcomplete',function(){
+    /**
+     * Code included inside this will only run once the page Document Object Model (DOM) is ready for JavaScript code to execute
+     * @see https://learn.jquery.com/using-jquery-core/document-ready/
+     */
+});
+
 // disable arrow keys changing radio selection
 $('input[type="radio"]').keydown(function(e)
 {
